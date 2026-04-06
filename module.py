4211ca08ca8761d7cef76dc03101412678c7a5d6
Sharath-ah-tech <sharath3012005@gmail.com>
2026-04-06 16:31:44 +0530

    first commit

---
+++ module.py
@@ -0,0 +1,17 @@
+def add(a, b):
+    return a + b
+print(add(a=5, b=6))
+def sub(a, b):
+    x = a-b
+    return x
+print(sub(6, 7))
+def mul(a, b):
+    return a*b
+def div(a, b):
+    return a//b 
+def calc():
+    m, d = mul(a=5, b=7), div(a = 10, b =5)
+    return m, d
+print(calc())
+
+
