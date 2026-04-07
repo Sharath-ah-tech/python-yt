38dd75249d09c565c2ee3cd0e44a01385613bc49
Sharath-ah-tech <sharath3012005@gmail.com>
2026-04-07 15:32:14 +0530

    feat : Builtin modules

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from module import add, sub, mul, div, calc
-def add_all(*data):
-    print(data)
-def sub_all(*d):
-    print(d)
-def info(**kwargs):
-    print(kwargs)
-def all(*data, **kwargs):
-    print(data, kwargs)
-print(add_all(add(5, 3), add(10, 2)))
-print(sub_all(sub(sub(10, 5),5)))
-print(info(name="sundar", age=45))
-print(all(add(5, 3), sub(10, 5), mul(2, 3), div(10, 2), name="sundar", age=45))
+import random
+from datetime import datetime, timedelta
+
+print(random.uniform(1, 100))
+print(random.random())
+ 
+print(random.randint(1, 100))
+print(random.randrange(1, 100, 2))
+print(random.choice(['apple', 'banana', 'cherry']))
+print(random.sample([1, 2, 3, 4, 5], 3))
+
+lst = [1, 2, 3, 4, 5]
+random.shuffle(lst)
+print(lst) 
+
+print(random.getrandbits(8))
+print(random.triangular(1, 10))
+print(random.gauss(0, 1))
+print(random.lognormvariate(0, 1))
+
+now = datetime.now()
+
+print(now)
+print(now.year, now.month, now.day)
+print(now.hour, now.minute, now.second)
+print(now.date(), now.time())
+
+print(now + timedelta(days=7))
