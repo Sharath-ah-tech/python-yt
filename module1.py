236000fd4bb05be8914f1fe28c2ca535b40ff4a2
Sharath-ah-tech <sharath3012005@gmail.com>
2026-04-07 15:00:17 +0530

    module1 and module are deleted

--- module1.py
+++
@@ -1,7 +0,0 @@
-from main import all
-from module import mul, div
-def operations():
-    m, d = mul(10, 5), div(5, 10)
-    return m, d
-print(operations())
-print(all(1,2,3,4,5,name="sundar", age= 45))
